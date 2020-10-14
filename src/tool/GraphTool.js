ab2c989659cceb62e34f20f4d8bdc755b38c735d
Alexander Staubo <alex@purefiction.net>
2020-10-14 01:37:55 -0400

    Lots of work.

--- src/tool/GraphTool.js
+++ src/tool/GraphTool.js
@@ -8,9 +8,7 @@ import {ForceGraph2D} from 'react-force-graph'
 import {v4 as uuidv4} from 'uuid'
 import BezierEasing from 'bezier-easing'
 
-// import styles from './GraphTool.css'
 import {useFetchDocuments, useListen} from './hooks'
-
 import styles from './GraphTool.css'
 
 const QUERY = `
@@ -25,6 +23,7 @@ const QUERY = `
 `
 
 const fadeEasing = BezierEasing(0, 0.9, 1, 1)
+const softEasing = BezierEasing(0.25, 0.1, 0.0, 1.0)
 const imageSize = 40
 
 function sortBy(array, f) {
@@ -36,7 +35,7 @@ function sortBy(array, f) {
 }
 
 function getTopDocTypes(counts) {
-  return sortBy(Object.keys(counts), (docType) => counts[docType] || 0)
+  return sortBy(Object.keys(counts), docType => counts[docType] || 0)
     .reverse()
     .slice(0, 10)
 }
@@ -75,8 +74,8 @@ function labelFor(doc) {
   return `${doc.title || doc.name || doc._id}`.trim()
 }
 
-function valueFor(node, maxSize) {
-  return 5 + 100 * (sizeOf(node.doc) / maxSize)
+function valueFor(doc, maxSize) {
+  return 5 + 100 * (sizeOf(doc) / maxSize)
 }
 
 function findRefs(obj, dest = []) {
@@ -118,10 +117,14 @@ function sizeOf(value) {
 }
 
 function loadImage(url) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     let img = new Image(imageSize, imageSize)
     img.onload = () => {
       resolve(img)
+    }
+    img.onerror = event => {
+      console.log('Image error', event)
+      resolve(null)
     }
     img.src = url
   })
@@ -151,9 +154,14 @@ class Users {
   _users = []
 
   async getById(id) {
-    let user = this._users.find((u) => u._id === id)
+    let user = this._users.find(u => u._id === id)
     if (!user) {
       user = await client.users.getById(id)
+
+      if (/\(Robot\)$/.test(user.displayName)) {
+        user = robotUsers[Date.now() % robotUsers.length]
+      }
+
       this._users.push(user)
 
       user.image = await loadImage(
@@ -168,6 +176,44 @@ class Users {
 const users = new Users()
 
 const idleTimeout = 10000
+
+const robotUsers = [
+  {
+    displayName: 'Ole',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/a7e0a0a03a59cccfd06d3bcdb5df71b75401bd28-1024x1024.png?w=80&h=80&fit=crop'
+  },
+  {
+    displayName: 'Jan-Tore',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/a79906968185c6e8a3658bcca42a79433222cc3e-1024x1024.png?w=80&h=80&fit=crop'
+  },
+  {
+    displayName: 'Stein',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/e579ed13f214ccc1e158ed42f46a30935afeb9e0-1024x1024.png?w=80&h=80&fit=crop'
+  },
+  {
+    displayName: 'Elvis',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/be36eb26194cb3ae3e218b1cf01d7c6c3425cf65-1024x1024.png?w=80&h=80&fit=crop'
+  },
+  {
+    displayName: 'Amanda',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/88e1da4f85f95856c9736aa4637e2bbf48ed6773-1024x1024.png?w=80&h=80&fit=crop'
+  },
+  {
+    displayName: 'Reidun',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/a79906968185c6e8a3658bcca42a79433222cc3e-1024x1024.png?w=80&h=80&fit=crop'
+  },
+  {
+    displayName: 'Tina',
+    imageUrl:
+      'https://cdn.sanity.io/images/ze41ydzg/production/6f6df5d7cc72f13988ad05ee026b567985bf1cf2-1024x1024.png?w=80&h=80&fit=crop'
+  }
+]
 
 class EditSession {
   user = null
@@ -185,20 +231,22 @@ class GraphData {
     }
 
     this.data = {
-      nodes: docs.map((d) => Object.assign({id: d._id, type: 'document', doc: d})),
+      nodes: docs.map(d => Object.assign({id: d._id, type: 'document', doc: d})),
       links: docs
-        .flatMap((doc) => findRefs(doc).map((ref) => ({source: doc._id, target: ref})))
-        .filter((link) => docsById[link.source] && docsById[link.target]),
+        .flatMap(doc => findRefs(doc).map(ref => ({source: doc._id, target: ref})))
+        .filter(link => docsById[link.source] && docsById[link.target])
     }
   }
 
   setEditSession(user, docNode) {
-    let session = this.sessions.find((s) => s.user.id === user.id && s.doc._id === docNode.doc._id)
+    let session = this.sessions.find(s => s.user.id === user.id && s.doc._id === docNode.doc._id)
     if (!session) {
       session = new EditSession()
       session.id = uuidv4()
       session.user = user
+      session.startTime = Date.now()
       session.doc = docNode.doc
+      session.angle = Math.random() * 2 * Math.PI
       this.sessions.push(session)
     }
     session.lastActive = Date.now()
@@ -219,7 +267,7 @@ class GraphData {
     Object.assign(copy, this)
     copy.data = {
       nodes: [...this.data.nodes],
-      links: [...this.data.links],
+      links: [...this.data.links]
     }
     return copy
   }
@@ -268,7 +316,7 @@ export function GraphTool() {
   const [docTypes, setDocTypes] = useState([])
   const [graph, setGraph] = useState(() => new GraphData())
 
-  const fetchCallback = useCallback((docs) => {
+  const fetchCallback = useCallback(docs => {
     docs = deduplicateDrafts(docs)
     setMaxSize(Math.max(...docs.map(sizeOf)))
     setDocuments(docs)
@@ -277,7 +325,7 @@ export function GraphTool() {
   }, [])
 
   const listenCallback = useCallback(
-    async (update) => {
+    async update => {
       console.log('Update:', update)
 
       const doc = update.result
@@ -291,7 +339,7 @@ export function GraphTool() {
 
         let oldDoc
         const docs = [...documents]
-        const idx = documents.findIndex((d) => d._id === doc._id)
+        const idx = documents.findIndex(d => d._id === doc._id)
         if (idx >= 0) {
           oldDoc = docs[idx]
           docs[idx] = doc
@@ -305,20 +353,18 @@ export function GraphTool() {
         const newGraph = graph.clone()
         let graphChanged = false
 
-        const oldRefs = findRefs(oldDoc || {}).filter(
-          (id) => id === doc._id || docsById[id] != null
-        )
-        const newRefs = findRefs(doc).filter((id) => id === doc._id || docsById[id] != null)
+        const oldRefs = findRefs(oldDoc || {}).filter(id => id === doc._id || docsById[id] != null)
+        const newRefs = findRefs(doc).filter(id => id === doc._id || docsById[id] != null)
 
         if (!deepEqual(oldRefs, newRefs)) {
           graphChanged = true
           newGraph.data.links = newGraph.data.links
-            .filter((l) => l.source.id !== doc._id)
-            .concat(newRefs.map((ref) => ({source: doc._id, target: ref})))
+            .filter(l => l.source.id !== doc._id)
+            .concat(newRefs.map(ref => ({source: doc._id, target: ref})))
         }
 
         let docNode
-        const nodeIdx = graph.data.nodes.findIndex((n) => n.doc && n.doc._id === doc._id)
+        const nodeIdx = graph.data.nodes.findIndex(n => n.doc && n.doc._id === doc._id)
         if (nodeIdx >= 0) {
           docNode = graph.data.nodes[nodeIdx]
           docNode.doc = doc
@@ -336,16 +382,16 @@ export function GraphTool() {
       } else if (update.transition === 'disappear') {
         const docId = stripDraftId(update.documentId)
 
-        const docs = documents.filter((d) => d._id !== docId)
+        const docs = documents.filter(d => d._id !== docId)
         setDocuments(docs)
         setDocTypes(getDocTypeCounts(docs))
         setMaxSize(Math.max(...docs.map(sizeOf)))
 
         const newGraph = graph.clone()
         newGraph.data.links = newGraph.data.links.filter(
-          (l) => l.source.id !== docId && l.target.id !== docId
+          l => l.source.id !== docId && l.target.id !== docId
         )
-        newGraph.data.nodes = newGraph.data.nodes.filter((n) => n.id !== docId)
+        newGraph.data.nodes = newGraph.data.nodes.filter(n => n.id !== docId)
         setGraph(newGraph)
       }
     },
@@ -365,7 +411,7 @@ export function GraphTool() {
   return (
     <div className={styles.root} style={{background: color.black.hex}}>
       <div className={styles.legend}>
-        {getTopDocTypes(docTypes).map((docType) => (
+        {getTopDocTypes(docTypes).map(docType => (
           <div
             className={styles.legend__row}
             key={docType}
@@ -383,20 +429,24 @@ export function GraphTool() {
         nodeAutoColorBy="group"
         numDimensions={2}
         enableNodeDrag={false}
-        onNodeHover={(node) => setHoverNode(node)}
+        onNodeHover={node => setHoverNode(node)}
+        onNodeClick={node => {
+          console.log('Click', node)
+          //window.location.href = `/desk/blog;publishedPosts;f4a58dd5-a8e7-4789-ad10-6855fa006452`
+        }}
         linkColor={() => rgba(color.gray[500].hex, 0.25)}
         nodeLabel={() => null}
         nodeRelSize={1}
-        nodeVal={(node) => valueFor(node, maxSize)}
+        nodeVal={node => valueFor(node.doc, maxSize)}
         onRenderFramePost={(ctx, globalScale) => {
           const frame = frameCounter++
 
           if (hoverNode) {
-            drawTooltip(ctx, globalScale, hoverNode, valueFor(hoverNode, maxSize))
+            // drawTooltip(ctx, globalScale, hoverNode, valueFor(hoverNode.doc, maxSize))
           }
 
           for (let session of graph.sessions) {
-            const node = graph.data.nodes.find((n) => n.doc && n.doc._id === session.doc._id)
+            const node = graph.data.nodes.find(n => n.doc && n.doc._id === session.doc._id)
             if (node) {
               const idleFactorRange = idleTimeout
 
@@ -404,103 +454,137 @@ export function GraphTool() {
               ctx.globalAlpha = fadeEasing(
                 1 - Math.min(idleFactorRange, Date.now() - session.lastActive) / idleFactorRange
               )
-              ctx.font = `${Math.round(12 / globalScale)}px sans-serif`
+              ctx.font = `bold ${Math.round(12 / globalScale)}px sans-serif`
 
-              const orient = hashStringToInt(session.id) % 2 === 0
+              const angle = session.angle
               const label = truncate(labelFor(session.doc), 30)
               const textMetrics = ctx.measureText(label)
-
-              const imgMargin = 10 / globalScale
-              const labelMarginY = 5 / globalScale
-              const w = textMetrics.width + imgMargin
-              const x = orient ? node.x - w : node.x + labelMarginY
-              const y = node.y
+              const radius = Math.sqrt(valueFor(node.doc, maxSize))
               const image = session.user.image
-              const imgW = image ? image.width / globalScale : 0
-              const imgH = image ? image.height / globalScale : 0
-              const imgX = orient ? x - imgW - imgMargin : x + w + imgMargin
-              const imgY = y - imgH * 0.7
-              const radius = Math.sqrt(valueFor(node, maxSize)) * globalScale
+              const userColor = userColorManager.get(session.user.displayName).tints[400].hex
+              const distance = radius + 40
+
+              const x = node.x + Math.sin(angle) * (distance / globalScale)
+              const y = node.y + Math.cos(angle) * (distance / globalScale)
+
+              const imgW = image ? image.width : 0
+              const imgH = image ? image.height : 0
 
               ctx.beginPath()
               ctx.strokeStyle = rgba(color.white.hex, 1.0)
-              ctx.lineWidth = 1 / globalScale
-              if (orient) {
-                ctx.moveTo(x, y)
-                ctx.lineTo(node.x, node.y)
-              } else {
-                ctx.moveTo(x + w, y)
-                ctx.lineTo(node.x, node.y)
-              }
+              ctx.lineWidth = 2 / globalScale
+              ctx.moveTo(
+                node.x + (Math.sin(angle) * (distance - imgW / 2)) / globalScale,
+                node.y + (Math.cos(angle) * (distance - imgH / 2)) / globalScale
+              )
+              ctx.lineTo(node.x + Math.sin(angle) * radius, node.y + Math.cos(angle) * radius)
               ctx.stroke()
 
-              if (session.doc) {
-                ctx.fillStyle = rgba(color.white.hex, 1.0)
-                ctx.textAlign = 'left'
-                ctx.textBaseline = 'bottom'
-                if (orient) {
-                  ctx.fillText(label, x, y - labelMarginY, w)
-                } else {
-                  ctx.fillText(label, x + w - textMetrics.width, y - labelMarginY, w)
-                }
-              }
+              // if (session.doc) {
+              //   ctx.fillStyle = rgba(color.white.hex, 1.0)
+              //   ctx.textAlign = 'left'
+              //   ctx.textBaseline = 'bottom'
+              //   if (orient) {
+              //     ctx.fillText(label, x, y - labelMarginY, w)
+              //   } else {
+              //     ctx.fillText(label, x + w - textMetrics.width, y - labelMarginY, w)
+              //   }
+              // }
 
               ctx.beginPath()
-              ctx.fillStyle = rgba(
-                color.white.hex,
-                Math.max(0.5 + Math.sin((frame / 20) % Math.PI) * 0.5, 0)
-              )
-              ctx.arc(
-                node.x,
-                node.y,
-                Math.max(radius * 0.5 + Math.sin((frame / 20) % Math.PI) * 2, 1) / globalScale,
-                0,
-                2 * Math.PI,
-                false
-              )
-              ctx.fill()
+              ctx.strokeStyle = rgba(color.white.hex, 1.0)
+              ctx.lineWidth = 2 / globalScale
+              ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI, false)
+              ctx.stroke()
 
               if (image) {
                 ctx.save()
+
+                const dur = 700
+                const f = softEasing(Math.max(0, (dur - (Date.now() - session.startTime)) / dur))
+                if (f > 0) {
+                  ctx.beginPath()
+                  ctx.fillStyle = rgba(userColor, f)
+                  ctx.arc(x, y, (imgW / 2 + 10) / globalScale, 0, 2 * Math.PI, false)
+                  ctx.fill()
+                }
+
                 ctx.beginPath()
                 ctx.fillStyle = rgba(color.white.hex, 1.0)
-                ctx.arc(imgX + imgW / 2, imgY + imgH / 2, imgW / 2, 0, 2 * Math.PI, false)
+                ctx.arc(x, y, imgW / globalScale / 2, 0, 2 * Math.PI, false)
                 ctx.clip()
 
-                ctx.drawImage(image, imgX, imgY, imgW, imgH)
+                ctx.drawImage(
+                  image,
+                  x - imgW / globalScale / 2,
+                  y - imgH / globalScale / 2,
+                  imgW / globalScale,
+                  imgH / globalScale
+                )
 
                 ctx.strokeStyle = color.black.hex
-                ctx.lineWidth = 5 / globalScale
+                ctx.lineWidth = 6 / globalScale
                 ctx.stroke()
-                ctx.strokeStyle = userColorManager.get(session.user.id).tints[400].hex
+
+                ctx.strokeStyle = userColor
                 ctx.lineWidth = 4 / globalScale
                 ctx.stroke()
+
                 ctx.restore()
               }
 
               ctx.beginPath()
               ctx.strokeStyle = rgba(color.black.hex, 1)
               ctx.lineWidth = 0.5 / globalScale
-              ctx.arc(imgX + imgW / 2, imgY + imgH / 2, imgW / 2, 0, 2 * Math.PI, false)
+              ctx.arc(x, y, imgW / globalScale / 2, 0, 2 * Math.PI, false)
               ctx.stroke()
+
+              const above = angle >= Math.PI / 2 && angle < Math.PI * 1.5
+              const textY = above
+                ? y - (imgH / 2 + 5) / globalScale
+                : y + (imgH / 2 + 5) / globalScale
+              ctx.fillStyle = rgba(color.white.hex, 1.0)
+              ctx.textAlign = 'center'
+              ctx.textBaseline = above ? 'bottom' : 'top'
+              ctx.fillText(session.user.displayName, x, textY)
 
               ctx.restore()
             }
           }
         }}
-        nodeCanvasObject={(node, ctx) => {
+        nodeCanvasObject={(node, ctx, globalScale) => {
           switch (node.type) {
             case 'document': {
               const nodeColor = getDocTypeColor(node.doc._type)
-              const radius = Math.sqrt(valueFor(node, maxSize))
+              const radius = Math.sqrt(valueFor(node.doc, maxSize))
+              const fontSize = Math.min(100, 10.0 / globalScale)
 
               ctx.beginPath()
-              ctx.fillStyle = nodeColor.fill
+              ctx.fillStyle =
+                hoverNode != null && node.doc._id === hoverNode.doc._id
+                  ? rgba(color.gray[500].hex, 0.8)
+                  : nodeColor.fill
               ctx.strokeStyle = nodeColor.border
               ctx.lineWidth = 0.5
               ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI, false)
               ctx.stroke()
               ctx.fill()
+
+              if (radius * globalScale > 10) {
+                ctx.font = `${fontSize}px sans-serif`
+                let w = radius * 2 + 30 / globalScale
+                for (let len = 50; len >= 5; len /= 1.2) {
+                  const label = truncate(labelFor(node.doc), Math.round(len))
+                  const textMetrics = ctx.measureText(label)
+                  if (textMetrics.width < w) {
+                    // ctx.fillStyle = rgba(color.white.hex, 1.0)
+                    ctx.textAlign = 'center'
+                    ctx.textBaseline = 'top'
+                    ctx.fillText(label, node.x, node.y + radius + 5 / globalScale)
+                    break
+                  }
+                }
+              }
             }
           }
         }}
@@ -531,7 +615,7 @@ function getDocTypeColor(docType) {
 
   colorCache[docType] = {
     fill: color[hue][400].hex,
-    border: rgba(color.black.hex, 0.5),
+    border: rgba(color.black.hex, 0.5)
   }
 
   return colorCache[docType]
