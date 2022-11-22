1c40d3ef18928e3521eac79e725ff72737a72b2e
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-11-22 17:39:52 +0100

    fix(ci): publish using semantic-release

--- src/tool/hooks.js
+++ src/tool/hooks.js
@@ -3,7 +3,7 @@ import client from 'part:@sanity/base/client'
 
 export function useListen(query, params, options, onUpdate, dependencies) {
   useEffect(() => {
-    const subscription = client.listen(query, params, options).subscribe(update => {
+    const subscription = client.listen(query, params, options).subscribe((update) => {
       onUpdate(update)
     })
     return () => {
@@ -14,7 +14,7 @@ export function useListen(query, params, options, onUpdate, dependencies) {
 
 export function useFetchDocuments(query, onFetch, dependencies) {
   useEffect(() => {
-    client.fetch(query).then(result => {
+    client.fetch(query).then((result) => {
       onFetch(result)
     })
   }, dependencies)
